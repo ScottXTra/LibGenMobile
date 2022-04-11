cdd2cffe21262d16b2088f25caa7772755216a7c
Karan Swatch <karanswatch@Karans-MacBook-Pro.local>
2022-04-11 00:05:35 -0400

    fixed broken import

--- mobile_app/lib/viewing pages/library_page.dart
+++ mobile_app/lib/viewing pages/library_page.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import '../attractions.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
